c59c38e840bb7c27618f59788f461dd504246c14
Grephon2512 <grephon777@gmail.com>
2026-02-11 14:30:23 +0530

    minor changes 1.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-'use client'
+"use client";
 
-import { Navbar } from '@/components/Navbar'
-import { Hero } from '@/components/Hero'
-import { ThreatSection } from '@/components/ThreatSection'
-import { AISection } from '@/components/AISection'
-import { HoneypotSection } from '@/components/HoneypotSection'
-import { AudioVideoSection } from '@/components/AudioVideoSection'
-import { EvidenceSection } from '@/components/EvidenceSection'
-import { MarketSection } from '@/components/MarketSection'
-import { FinalCTA } from '@/components/FinalCTA'
+import { Navbar } from "@/components/Navbar";
+import { Hero } from "@/components/Hero";
+import { ThreatSection } from "@/components/ThreatSection";
+import { AISection } from "@/components/AISection";
+import { HoneypotSection } from "@/components/HoneypotSection";
+import { AudioVideoSection } from "@/components/AudioVideoSection";
+import { EvidenceSection } from "@/components/EvidenceSection";
+import { MarketSection } from "@/components/MarketSection";
+import { FinalCTA } from "@/components/FinalCTA";
 
 export default function Page() {
   return (
@@ -29,5 +29,5 @@ export default function Page() {
       </section>
       <FinalCTA />
     </main>
-  )
+  );
 }
